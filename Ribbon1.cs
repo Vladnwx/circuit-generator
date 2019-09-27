bab9f0674daea4e13a7800f0092e077dd1ea307b
Vlad <anykey196@gmail.com>
2019-09-27 09:26:48 +0500

    Добавлены списки со свойствами кабеля

--- Ribbon1.cs
+++ Ribbon1.cs
@@ -45,7 +45,37 @@ namespace circuit_generator
             MessageBox.Show(nativeWorksheet.Name); //Вывод названия текущего листа
         }
     }
-    
+    public class cable ()    {
+        SortedList<string, string> conductor_material = //
+            new SortedList<string, string>(); // Сортированный список материалов жил кабеля
+        conductor_material.Add("cu", "медь");
+        conductor_material.Add("al", "алюминий");
+        
+        SortedList<string, string> insulation_material = 
+            new SortedList<string, string>(); //Сортированный список материалов изоляции кабеля
+        insulation_material.Add("pvh", "ПВХ");
+        insulation_material.Add("pe", "ПЭ");
+        
+        SortedList<string, string>  environment = 
+            new SortedList<string, string>(); //Сортированный список среды, в которой прокладывается кабель
+         environment.Add("air", "воздух");
+         environment.Add("ground", "земля");
+     
+        SortedList<string, string>  current_type = 
+            new SortedList<string, string>(); //Сортированный список рода тока
+         current_type.Add("dc", "постоянный");
+         current_type.Add("ac", "переменный");
+      
+        SortedList<string, string>  core_сonstruction = 
+            new SortedList<string, string>(); //Сортированный список конструкций жилы
+         core_сonstruction.Add("mo", "однопроволочный");
+         core_сonstruction.Add("mn", "многопроволочный");
+        
+        SortedList<string, string>  number_of_cores_in_cable = 
+            new SortedList<string, string>(); //Сортированный список количество жил в кабеле
+         number_of_cores_in_cable.Add("odnojil", "одножильный");
+         number_of_cores_in_cable.Add("mnogojil", "многожильный");
+     }
     public class fider ()
     {
         public string Source {get; set;} // Источник (начало фидера)
