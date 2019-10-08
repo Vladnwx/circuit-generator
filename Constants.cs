d44e6a40d1aca56920e3b44a825fd3ac0edd9584
Vlad <anykey196@gmail.com>
2019-10-08 11:44:07 +0500

    Исправил коментарий

--- Constants.cs
+++ Constants.cs
@@ -10,7 +10,7 @@ namespace circuit_generator
         public const int Tabl_start_column = 4; // Номер стобца, с которого начинается таблица схемы (столбец Е)
         public const int First_fider_column = Tabl_start_column+2; // Номер стобца, с которого начинается фидер
         public const int fider_width_column = 2; // Число столбцов, которое занимает фидер
-        public const int Width_column = 11; // Число столбцов, которое занимает фидер
+        public const int Width_column = 11; // Ширина столбца
         public const int Tabl_start_row = 4; // Номер строки, с которого начинается таблица схемы (срока 4)
         public const int First_fider_row = Tabl_start_row+11; // Номер строки, с которой начинается фидер
         public const int Last_fider_row = Tabl_start_row+60; // Номер строки, которой заканчивается фидер
