539f283843a235d85505876ddc8e8a795f21391d
anykey196 <anykey196@gmail.com>
2019-10-29 21:00:18 +0500

    косметические исправления

--- fider/Fider.cs
+++ fider/Fider.cs
@@ -3,56 +3,41 @@ using System.Windows.Forms;
 
 namespace circuit_generator
 {
-    public class Fider
+    public partial class Fider : Nagruzka
     {
-
-        public string Source { get; set; } // Источник (начало фидера)
-        public string Destination { get; set; } // Место назначения (конец фидера)
         public double Lenght { get; set; } // Длина кабельной линии
-
-        public double Power { get; set; } // Мощность фидера
-        public double Current { get; set; } // Ток фидера
-        public double Voltage { get; set; } // Напряжение фидера
-        public double Cos_fi { get; set; } // Косинус фидера
-
         public int Fider_number_coluumn { get; set; } // Номер столбца для фидера
-
         public Fider() : this("Неизвестно") // Конструктор без параметров
         {
         }
-        public Fider(string Source) : this(Source, "Неизвестно") // Конструктор с указанием только источника
+        public Fider(string Start) : this(Start, "Неизвестно") // Конструктор с указанием только источника
         {
         }
-        public Fider(string Source, string destination) : this(Source, destination, 10) // Конструктор без длины
+        public Fider(string Start, string Source) : this(Source, Source, 10) // Конструктор без длины
         {
         }
-        public Fider(string Source, string destination, double lenght) // Конструктор полный
+        public Fider(string Start, string Source, double lenght) // Конструктор полный
         {
+            this.Start = Start;
             this.Source = Source;
-            this.Destination = destination;
             this.Lenght = lenght;
         }
         public void GetInfo() //Информация о фидере в консоль
         {
-            Console.WriteLine($"Источник: {Source}  Место назначения: {Destination} Длина кабельной линии: {Lenght}");
+            Console.WriteLine($"Источник: {Start}  Место назначения: {Source} Длина кабельной линии: {Lenght}");
         }
-
         public void Calculate_Power()  //Вычисляет мощность фидера в зависимости от нагрузки  и если нужно то суммирует
         {
             //Power = 1;
         }
-
         public void Calculate_current()  //Вычисляет ток в фидере, по мощности и количеству фаз
         {
             if (this.Voltage < 240)
-                Current = (Power * 0.001) / (Voltage * Cos_fi);
+                Current = (Power * 0.001) / (Voltage * Cosphi);
             else if (Voltage > 240 && Voltage < 410)
-                Current = (Power * 0.001) / (Voltage * Cos_fi * 1.73);
+                Current = (Power * 0.001) / (Voltage * Cosphi * 1.73);
             else MessageBox.Show(text: "Ошибка"); // Exeption Вывод ошибки о недопустимости напряжения
-
         }
-
-
         public void Select_QF_current()  //Выбирает автомат по мощности фидера, добавить возможность ручного выбора автомата
         {
             // Проверка что ток не равен нулю и если он равен нулю то писать резерв
@@ -61,44 +46,34 @@ namespace circuit_generator
             }
             else
                 MessageBox.Show("Ошибка"); // Exeption Вывод ошибки тока
-
         }
-
         public void Select_cabel_current()  //Выбирает Кабель по мощности и току фидера фидера, добавить возможность ручного выбора кабеля
         {
             //Проверка что ток не равен нулю  
         }
-
         public void Calculate_cabel_loss()  //Вычисляет потери по выбранному кабелю и току фидера фидера если потери кабеля превышают уставку, то выбирает следующий кабель и автомат
         {
         }
-
         public void Output_fider_info1()  //Объединяет информацию по фидеру в 1-ю строчку надписи кабеля
         {
             //Output_fider_info1() = 
         }
-
         public void Output_fider_info2()  //Объединяет информацию по фидеру во 2-ю строчку надписи кабеля
         {
             // Output_fider_info2() = 
         }
-
         public void Select_cabel_truba()  //Выбирает сечение применяемой трубы для фидера
         {
         }
-
         public void Numeration_QF()  //Автоматическая нумерация автоматов, добавить возможность ручного выбора номера, остальные автонумерация
         {
         }
-
         public void Sort_Fider_current()  //Автоматическая сортировка автоматов по нагрузке и балансирование нагрузки
         {
         }
-
         public void Calculate_tkz_R()  //Вычисляет Эквивалетное значение омического сопротивления фидера для ТКЗ
         {
         }
-
         public void Calculate_tkz_X()  //Вычисляет Эквивалетное значение реактивного сопротивления линии для ТКЗ
         {
         }
--- nagruzka/Nagruzka.cs
+++ nagruzka/Nagruzka.cs
@@ -3,18 +3,18 @@
     public partial class Nagruzka
     {
         Microsoft.Office.Interop.Excel.Worksheet Worksheet { get; set; } // Лист, с которым мы работаем
-        private double NumbersOfPhases { get; set; } // Число фаз
-        private double NumberPhase { get; set; } // Номер фазы
-        private double Voltage { get; set; } // Напряжение в Вольтах
-        private double Power { get; set; } // Мощность в кВт, добавить проверку мощности (она не может быть отрицательной)
-        private double Cosphi { get; set; } // Косинус нагрузки , добавить проверку косинуса (он не может быть больше+-1)
-        private double Current { get; set; } // Косинус нагрузки , добавить проверку косинуса (он не может быть больше+-1)
-        private bool StartInBox { get; set; } // Начало нагрузки в щите или ответвление от фидера, добавить проверку, что при отсутствии фидера невозможно поставить нагрузку как начинающуюся не от щита
-        private string Start { get; set; } // начало нагрузки
-        private string Source { get; set; } // Местоположение нагрузки
-        private string Harakter1 { get; set; } // Характер нагрузки
-        private string TypeNetwork1 { get; set; } // Тип сети
-        private string Type1 { get; set; } // Тип нагрузки промежуточная/конечная
-        private int ActiveColuumn { get; set; } // Номер столбца для нагрузки
+        protected double NumbersOfPhases { get; set; } // Число фаз
+        protected double NumberPhase { get; set; } // Номер фазы
+        protected double Voltage { get; set; } // Напряжение в Вольтах
+        protected double Power { get; set; } // Мощность в кВт, добавить проверку мощности (она не может быть отрицательной)
+        protected double Cosphi { get; set; } // Косинус нагрузки , добавить проверку косинуса (он не может быть больше+-1)
+        protected double Current { get; set; } // Косинус нагрузки , добавить проверку косинуса (он не может быть больше+-1)
+        protected bool StartInBox { get; set; } // Начало нагрузки в щите или ответвление от фидера, добавить проверку, что при отсутствии фидера невозможно поставить нагрузку как начинающуюся не от щита
+        protected string Start { get; set; } // начало нагрузки
+        protected string Source { get; set; } // Местоположение нагрузки
+        protected string Harakter1 { get; set; } // Характер нагрузки
+        protected string TypeNetwork1 { get; set; } // Тип сети
+        protected string Type1 { get; set; } // Тип нагрузки промежуточная/конечная
+        protected int ActiveColuumn { get; set; } // Номер столбца для нагрузки
     }
 }
