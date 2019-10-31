216a689cf5e1036ef4fc79911686af5a88a2c5d3
Vladnwx <savelevvn@eletech.local>
2019-10-31 09:17:27 +0500

    добавил foreach

--- nagruzka/GetFromSheet.cs
+++ nagruzka/GetFromSheet.cs
@@ -9,19 +9,30 @@ namespace circuit_generator
         {
             this.Worksheet = Globals.ThisAddIn.Application.ActiveSheet;
             this.ActiveColuumn = Globals.ThisAddIn.Application.ActiveCell.Column;
-
-
             try
             {
-                //StandartNumbersOfPhases = Worksheet.Cells[Constants.Fider.Row.Phase, ActiveColuumn].Value;
-
-                Power = this.Worksheet.Cells[Constants.Fider.Row.P, this.ActiveColuumn].Value;
+            //StandartNumbersOfPhases = Worksheet.Cells[Constants.Fider.Row.Phase, ActiveColuumn].Value;
+            NagruzkaList.Add(Constants.Fider.Row.Phase, this.NumbersOfPhases);
+            NagruzkaList.Add(Constants.Fider.Row.P, this.Power);
+            NagruzkaList.Add(Constants.Fider.Row.U, this.Voltage);
+            NagruzkaList.Add(Constants.Fider.Row.Cos, this.Cosphi);
+            NagruzkaList.Add(Constants.Fider.Row.I, this.Current);
+            NagruzkaList.Add(Constants.Fider.Row.Start, this.Start);    
+            NagruzkaList.Add(Constants.Fider.Row.Finish, this.Source);     
+            NagruzkaList.TrimExcess();
+            ICollection<int> keys = NagruzkaList.Keys;
+            foreach (int c in keys)
+            {
+                NagruzkaList[c] = Worksheet.Cells[c, ActiveColuumn].Value;
+            }
+               /*  Power = this.Worksheet.Cells[Constants.Fider.Row.P, this.ActiveColuumn].Value;
 
                 Cosphi = this.Worksheet.Cells[Constants.Fider.Row.Cos, this.ActiveColuumn].Value;
 
                 Start = this.Worksheet.Cells[Constants.Fider.Row.Start, this.ActiveColuumn].Value;
 
                 Source = this.Worksheet.Cells[Constants.Fider.Row.Finish, this.ActiveColuumn].Value;
+           */
             }
 
             catch (Exception ex)
